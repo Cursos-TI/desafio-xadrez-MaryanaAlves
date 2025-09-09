56559db78db52e4d93d71cddd6e049930fefbc06
Maryana Alves <153780901+MaryanaAlves@users.noreply.github.com>
2025-09-09 15:15:12 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
+    int bispo=0, torre, rainha=0;
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    printf("### Movimento das peças de xadrez ###\n\n");
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    //movimentação da peça Torre cinco casas para a direita.
+    printf("Peça: Torre\n");
+    for(torre=0; torre<5; torre++){
+        printf("Direita\n");
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    //movimentação da peça Bispo cinco casas na diagonal para cima e à direita.
+    printf("\nPeça: Bispo\n");
+    while(bispo<5){
+        printf("Cima\n");
+        printf("Direita\n");
+        bispo++;
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    //movimentação da peça Rainha oito casas para a esquerda.
+    printf("\nPeça: Rainha\n");
+    do{
+        printf("Esquerda\n");
+        rainha++;
+    } while(rainha<8);
+
+    return 0;
+}
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
@@ -27,6 +39,3 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
-}
