92c882f4615c8581ac3188e700d50d872a020c6a
Maryana Alves <153780901+MaryanaAlves@users.noreply.github.com>
2025-09-11 15:09:11 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 
+void torre(int casas){    
+    if(casas>0){
+        printf("Direita\n");
+        torre(casas-1);
+    }
+}
+
+void bispo(int casas){
+    if (casas>0){
+        for(int v = 0; v < 1; v++){
+            printf("Cima\n");
+            for(int h = 0; h < 1; h++){
+                printf("Direita\n");
+            }
+        }
+        bispo(casas-1);
+    }
+}
+
+void rainha(int casas){
+    if(casas>0){
+        printf("Esquerda\n");
+        rainha(casas-1);
+    }
+}
+
 int main(){
-    int bispo=0, torre, rainha=0, cavalo;
-
-    printf("### Movimento das peças de xadrez ###\n\n");
+    printf("\nPeça: Torre\n");
+    torre(5);
     
-    printf("Peça: Torre\n");
-    for(torre=0; torre<5; torre++){
-        printf("Direita\n");
-    }
-
     printf("\nPeça: Bispo\n");
-    while(bispo<5){
-        printf("Cima\n");
-        printf("Direita\n");
-        bispo++;
-    }
+    bispo(5);
 
     printf("\nPeça: Rainha\n");
-    do{
-        printf("Esquerda\n");
-        rainha++;
-    } while(rainha<8);
+    rainha(8);
 
     printf("\nPeça: Cavalo\n");
-    for(cavalo=1; cavalo<=2; cavalo++){
-        printf("Baixo\n");
-        while(cavalo==2){
-            printf("Esquerda\n");
+    for(int m=0, movimentos=1; m < movimentos; m++){
+        for(int v=1; v<=2; v++){
+            printf("Cima\n");
+        }
+        for(int h = 0; h < 1; h++){
+            printf("Direita\n");
             break;
         }
     }
+
     return 0;
 }
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
